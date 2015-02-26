5f06c7d36ec86fc4edf884d6f9266009a876212b
Asparanc <alexis.del@free.fr>
2015-02-26 18:33:09 +0100

    Create the reset account extension

--- Asparanc/EconomyMaster/ResetAccount.php
+++ Asparanc/EconomyMaster/ResetAccount.php
@@ -1,1 +1,26 @@
+<?php
+namespace Asparanc\EconomyMaster;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\permission\Permission;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\TextFormat;
+use Asparanc\EconomyMaster\EcoAPI;
+class ResetAccount implements Listener{
+public function __construct(EcoAPI $plugin){
+  $this->plugin = $plugin;
+ }
 
+public function ResetPlayer($player){
+  if(EcoAPI::getInstance()->isRegistered($player)){
+     $this->data = $this->getDataFolder();
+    	  $data = new Config($this->data . "players/" . strtolower($player->getName() . ".yml"), Config::YAML);
+    	  $data->set("money", 0);
+    	  $data->set("bank", 0);
+    	$data->save();
+    	$player->sendMessage("[Eco] Your account have been reset.");
+  }
+}
+}
