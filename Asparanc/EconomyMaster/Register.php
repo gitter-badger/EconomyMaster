002591c8955b3e280e8238613c24625663d24421
Asparanc <alexis.del@free.fr>
2015-03-10 17:44:03 +0100

    Add 3 XP jobs :p

--- Asparanc/EconomyMaster/Register.php
+++ Asparanc/EconomyMaster/Register.php
@@ -21,7 +21,13 @@ public function onJoin(PlayerJoinEvent $event){
     	  $this->data = $this->getDataFolder();
     	  $data = new Config($this->data . "players/" . strtolower($player->getName() . ".yml"), Config::YAML);
     	$data->set("money", 0);
-    	$date->set("bank", 0);
+    	$data->set("bank", 0);
+    	$data->set("Miner", 0);
+    	$data->set("MinerXP", 0);
+    	$data->set("Woodcutter", 0);
+    	$data->set("WoodcutterXP", 0);
+    	$data->set("Farmer", 0);
+    	$data->set("FarmerXP", 0);
     	$data->save();
     	  $this->plugin->getLogger()->notice("New player : ".$player->getName());
     	}
